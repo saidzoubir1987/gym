6f90b82e71c9d1cfa2c7eb4b82ef70285cf55b3d
saidzoubir1987 <saidzoubir1987@gmail.com>
2026-02-18 13:43:03 +0000

    Update Firebase API key in firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+apiKey: "AIzaSyDxxxxxxxxxxxx"
+  authDomain: "micodiag.firebaseapp.com",
+  projectId: "micodiag",
+  storageBucket: "micodiag.firebasestorage.app",
+  messagingSenderId: "332330708550",
+  appId: "1:332330708550:web:75aeefc1a9e8dfc43be447"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
